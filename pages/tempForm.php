b64a4cd2512eba13fc0686ca7ee4d5658bf4ca88
telepiggy <Rocklobster1!>
2020-02-11 12:14:29 -0500

    Version 1.1

--- pages/tempForm.php
+++ pages/tempForm.php
@@ -21,7 +21,7 @@
                 </a>
 
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="./login.php">Login</a>
+                <a class="dropdown-item" href="./login.php">Login!</a>
                 </div>
             </div>
 
