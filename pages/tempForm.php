2b6d6665a0e09e97f88b2a41d6ca7387ede3f22b
telepiggy <Rocklobster1!>
2020-02-11 11:40:47 -0500

    Test

---
+++ pages/tempForm.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <div class="row">
+            <div class="col-md-6">
+                Boop
+            </div>
+            <div class="col-md-6">
+                Poob
+            </div>
+        </div>
+
+        <form >
+
+            <label>
+                Name<input type="text" name="name">
+            </label>
+
+            <label>
+                Age<input type="text" name="age">
+            </label>
+
+            <label>
+                Education<input type="text" name="education">
+            </label>
+
+        </form>
+    </body>
+</html>
